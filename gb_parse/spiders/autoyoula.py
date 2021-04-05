57cf9c6463b82c7af1f8d4592e6ef9a10793a88c
RMaturin <maturin@mail.ru>
2021-04-05 23:52:13 +0300

    [upd]: les04 HW

--- gb_parse/spiders/autoyoula.py
+++ gb_parse/spiders/autoyoula.py
@@ -1,4 +1,6 @@
 import scrapy
+import pymongo
+from base64 import b64decode
 
 
 class AutoyoulaSpider(scrapy.Spider):
@@ -11,6 +13,11 @@ class AutoyoulaSpider(scrapy.Spider):
         "pagination": "a.Paginator_button__u1e7D",
         "car": ".SerpSnippet_titleWrapper__38bZM a.SerpSnippet_name__3F7Yu",
     }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.db_client = pymongo.MongoClient("mongodb://localhost:27017")
+        self.db = self.db_client["gd_data_mining"]
 
     def _get_follow(self, response, select_str, callback, **kwargs):
         for a in response.css(select_str):
@@ -26,5 +33,49 @@ class AutoyoulaSpider(scrapy.Spider):
         yield from self._get_follow(response, self._css_selectors["car"], self.car_parse)
 
     def car_parse(self, response):
-        # title = response.css(".AdvertCard_advertTitle__1S1Ak::text").extract_first()
-        print(1)
+        data = {}
+        for key, funk in self._get_data_template().items():
+            try:
+                data[key] = funk(response)
+            except (AttributeError, ValueError) as exc:
+                print(exc)
+        self.db[self.name].insert_one(data)
+
+    def _get_data_template(self):
+        n = ".AdvertSpecs_label__2JHnS::text"
+        v1 = ".AdvertSpecs_data__xK2Qx a::text"
+        v2 = ".AdvertSpecs_data__xK2Qx::text"
+        return {
+            "title": lambda resp: resp.css(".AdvertCard_advertTitle__1S1Ak::text").extract_first(),
+            "image": lambda resp: resp.css(
+                "figure.PhotoGallery_photo__36e_r img::attr(src)"
+            ).extract(),
+            "characteristics": lambda resp: [
+                {
+                    "name": itm.css(n).extract_first(),
+                    "value": (itm.css(v1).extract_first() or itm.css(v2).extract_first()),
+                }
+                for itm in resp.css("div.AdvertCard_specs__2FEHc .AdvertSpecs_row__ljPcX")
+            ],
+            "descriptions": lambda resp: resp.css(
+                ".AdvertCard_descriptionInner__KnuRi::text"
+            ).extract_first(),
+            "author": lambda resp: self._get_data_from_script(resp).get("author"),
+            "phone": lambda resp: self._get_data_from_script(resp).get("phone"),
+        }
+
+    @staticmethod
+    def _get_data_from_script(response):
+        data = {}
+        for script in response.css("script::text").extract():
+            if "window.transitState = decodeURIComponent" in script:
+                start = script.rfind("youlaId%22%2C%22") + 16
+                end = script.find("%22%2C%22avatar")
+                if end == -1:
+                    end = script.find("%22%2C%22alias")
+                data["author"] = response.urljoin(f"/user/{script[start:end]}")
+
+                start = script.find("phone%22%2C%22") + 14
+                end = script.find("%3D%3D%22%2C%22")
+                data["phone"] = b64decode(b64decode(f"{script[start:end]}==")).decode("utf-8")
+        return data
