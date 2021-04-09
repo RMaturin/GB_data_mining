65f1af791201b7f570b60e8c586454362ec4356c
RMaturin <maturin@mail.ru>
2021-04-09 23:59:22 +0300

    [upd]: les05 HW hh done

--- gb_parse/loaders.py
+++ gb_parse/loaders.py
@@ -47,3 +47,29 @@ class AutoYoulaLoder(ItemLoader):
     descriptions_out = TakeFirst()
     phone_in = MapCompose(decode_phone)
     phone_out = TakeFirst()
+
+
+def flat_text(items):
+    return "".join(items).replace("\xa0", "")
+
+
+def hh_user_url(user_id):
+    return urljoin("https://hh.ru/", user_id)
+
+
+class HHLoader(ItemLoader):
+    default_item_class = dict
+    url_out = TakeFirst()
+    title_out = TakeFirst()
+    salary_out = flat_text
+    # description_in = flat_text
+    # description_out = flat_text,
+    author_in = MapCompose(hh_user_url)
+    author_out = TakeFirst()
+
+
+class HHEmpLoader(ItemLoader):
+    default_item_class = dict
+    url_out = TakeFirst()
+    name_out = flat_text
+    site_out = TakeFirst()
--- gb_parse/spiders/xpaths.py
+++ gb_parse/spiders/xpaths.py
@@ -31,18 +31,26 @@ AUTO_YOULA_CAR_XPATH = {
     },
 }
 
-# HH_PAGE_XPATH = {
-#     "pagination": '//div[@data-qa="pager-block"]//a[@data-qa="pager-page"]/@href',
-#     "vacancy": '//div[contains(@data-qa, "vacancy-serp__vacancy")]//'
-#     'a[@data-qa="vacancy-serp__vacancy-title"]/@href',
-# }
-#
-# HH_VACANCY_XPATH = {
-#     "title": '//h1[@data-qa="vacancy-title"]/text()',
-#     "salary": '//p[@class="vacancy-salary"]/span/text()',
-#     "description": '//div[@data-qa="vacancy-description"]//text()',
-#     "skills": '//div[@class="bloko-tag-list"]//'
-#     'div[contains(@data-qa, "skills-element")]/'
-#     'span[@data-qa="bloko-tag__text"]/text()',
-#     "author": '//a[@data-qa="vacancy-company-name"]/@href',
-# }
+HH_PAGE_XPATH = {
+    "pagination": '//div[@data-qa="pager-block"]//a[@data-qa="pager-page"]/@href',
+    "vacancy": '//div[contains(@data-qa, "vacancy-serp__vacancy")]//'
+    'a[@data-qa="vacancy-serp__vacancy-title"]/@href',
+}
+
+HH_VACANCY_XPATH = {
+    "title": '//h1[@data-qa="vacancy-title"]/text()',
+    "salary": '//p[@class="vacancy-salary"]/span/text()',
+    "description": '//div[@data-qa="vacancy-description"]//text()',
+    "skills": '//div[@class="bloko-tag-list"]//'
+    'div[contains(@data-qa, "skills-element")]/'
+    'span[@data-qa="bloko-tag__text"]/text()',
+    "author": '//a[@data-qa="vacancy-company-name"]/@href',
+}
+
+HH_AUTHOR_XPATH = {
+    "name": '//div[@class="company-header"]//h1/span[@class="company-header-title-name"]/text()',
+    "site": '//div[@class="employer-sidebar-content"]//a[@data-qa="sidebar-company-site"]/@href',
+    "activity": '//div[contains(text(), "Сферы деятельности")]/../p/text()',
+    "description": '//div[@data-qa="company-description-text"]//p/text()',
+    "vacancy": '//div[contains(text(), "Вакансии")]/../a[@data-qa="employer-page__employer-vacancies-link"]/@href',
+}
